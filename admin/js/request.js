9b472c708e22ebfdd39bc4ee2b023deb4f977832
Ga-chi <10255926802qq.com>
2019-12-30 08:46:51 +0800

    个人中心和封装url

--- admin/js/request.js
+++ admin/js/request.js
@@ -10,4 +10,12 @@
       }
     }
   });
+  const baseURL = "http://localhost:8080/api/v1";
+  var BigNew = {
+    user_login: baseURL + "/admin/user/login",
+    user_info: baseURL + "/admin/user/info",
+    user_detail: baseURL + "/admin/user/detail",
+    user_edit: baseURL + "/admin/user/edit"
+  };
+  window.BigNew = BigNew;
 })(window);
